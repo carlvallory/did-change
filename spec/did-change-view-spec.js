f739b569cbee24d118ae65866f9524588eb730c8
cvallory <carlvallory@gmail.com>
2018-01-01 12:49:01 -0300

    first commit

---
+++ spec/did-change-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DidChangeView from '../lib/did-change-view';
+
+describe('DidChangeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
